505ffb825d6d426bf9fb165fbaf913dd63945dc6
Maximilian Asam <Maximilian.Asam1@HS-Augsburg.DE>
2023-05-08 23:52:14 +0200

    added editable number component

---
+++ src/components/IO/EditableNumber.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+    modelValue: number
+    edit: boolean
+}>()
+const emit = defineEmits<{ (e: 'update:modelValue', value: number): void }>()
+
+const value = computed<number>({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emit('update:modelValue', value)
+    }
+})
+</script>
+
+<template>
+    <template v-if="edit">
+        <input type="text" v-model="value" />
+    </template>
+    <template v-else>
+        <span>{{ value }}</span>
+    </template>
+</template>
+
+<style></style>
