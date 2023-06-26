713f1ce35199f155d7600edb8f817acdbf7de9a5
Maximilian Asam <Maximilian.Asam1@HS-Augsburg.DE>
2023-06-27 01:15:15 +0200

    added what to eat view

--- src/views/WhatToEatView.vue
+++ src/views/WhatToEatView.vue
@@ -1,5 +1,62 @@
-<template>
-    <h1>This is a placeholder page</h1>
-</template>
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+import RecipeCard from '@/components/RecipeCard.vue'
+import type { SavedRecipe } from '@/types/recipe'
+import { onMounted, ref } from 'vue'
 
-<style></style>
+const userStore = useUserStore()
+const recipies = ref<SavedRecipe[]>([])
+
+onMounted(() => {
+    // request from backend
+    fetch(
+        'http://127.0.0.1:8000/recipies/?' +
+            new URLSearchParams({
+                order: 'random',
+                limit: '3'
+            }),
+        {
+            headers: {
+                Authorization: `Bearer ${userStore.token}`
+            }
+        }
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            recipies.value = data
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        })
+})
+</script>
+<template>
+    <div>
+        <div class="column">
+            <h1>What to eat?</h1>
+            <p>How about one of these?</p>
+            <div class="row">
+                <RecipeCard
+                    v-for="recipe in recipies"
+                    v-if="recipies.length"
+                    :key="recipe.id"
+                    :recipe="recipe"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.column {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+}
+</style>
